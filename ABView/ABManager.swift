a3cb1075d06cc94f48a25589bfaadb40a133ba44
Administrator <administrator@OITs-MacBook-Pro.local>
2018-02-13 15:44:20 +0530

    Initial commit

---
+++ ABView/ABManager.swift
@@ -0,0 +1,47 @@
+//
+//  Loader.swift
+//  AnimatedBackground
+//
+//  Created by Administrator on 13/02/18.
+//  Copyright © 2018 Order of the Light. All rights reserved.
+//
+
+import Foundation
+
+
+public class ABView{
+    
+    public init() {}
+    
+    public let view = Bundle.main.loadNibNamed("BackgroundView", owner: nil, options: nil)?.first as! UIView
+    
+    
+    public func loadBackgroundNib(inView v : UIView){
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.alpha = 0
+        v.addSubview(view)
+        
+        let dictViews = ["subview":view]
+        let verticalConst = NSLayoutConstraint.constraints(withVisualFormat: "V:|-(0)-[subview]-(0)-|", options: [], metrics: nil, views: dictViews)
+        let horizontalConst = NSLayoutConstraint.constraints(withVisualFormat:  "H:|-(0)-[subview]-(0)-|", options: [], metrics: nil, views: dictViews)
+        
+        v.addConstraints(verticalConst)
+        v.addConstraints(horizontalConst)
+        
+    }
+    
+    public func startAnimation(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            UIView.animate(withDuration: 1.0, delay: 0, options: [.repeat,.curveLinear,.autoreverse,.beginFromCurrentState], animations: {
+                self.view.alpha = 1.0
+            }, completion: { (_) in
+                
+            })
+        }
+    }
+    
+    
+    public func stopAnimation(){
+        view.layer.removeAllAnimations()
+    }
+}
---
+++ ABView/ABView.h
@@ -0,0 +1,19 @@
+//
+//  ABView.h
+//  ABView
+//
+//  Created by Administrator on 13/02/18.
+//  Copyright © 2018 Order of the Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ABView.
+FOUNDATION_EXPORT double ABViewVersionNumber;
+
+//! Project version string for ABView.
+FOUNDATION_EXPORT const unsigned char ABViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABView/PublicHeader.h>
+
+
